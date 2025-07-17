47cbfdd5eed0adfdc275170171a91edda77aa88b
sujal <sujalkesharwani220@gmail.com>
2025-07-17 22:37:12 +0530

    05: ai jobs

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,12 +1,8 @@
-
 import { serve } from "inngest/next";
 import { inngest } from "@/inngest/client";
-import { helloWorld } from "@/inngest/functions";
+import { inngestFunction } from "@/inngest/functions";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [
-    helloWorld
-    /* your functions will be passed here later! */
-  ],
+  functions: [inngestFunction],
 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,38 @@
 "use client";
-
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
 import { toast } from "sonner";
 
-export default function Home() {
+const Page = () => {
+  const [value, setValue] = useState("");
+
   const trpc = useTRPC();
   const invoke = useMutation(
     trpc.invoke.mutationOptions({
-      onSuccess: ({ success }) =>
-        toast.success(success),
+      onSuccess: ({ success }) => {
+        toast.success(success), 
+        setValue("");
+      },
       onError: (error) => toast.error(error.message),
     })
   );
 
   return (
-    <Button
-      disabled={invoke.isPending || invoke.isError}
-      onClick={() => invoke.mutate({ email: "test@gmail.com2" })}
-    >
-      Invoke trpc
-    </Button>
+    <div className="h-screen w-screen flex items-center justify-center bg-blue-950">
+      <div className="max-w-7xl mx-auto flex items-center flex-col gap-y-4 justify-center">
+        <Input value={value} onChange={(e) => setValue(e.target.value)} />
+        <Button
+          disabled={invoke.isPending}
+          onClick={() => invoke.mutate({ value: value })}
+        >
+          Submit
+        </Button>
+      </div>
+    </div>
   );
-}
+};
+
+export default Page;
