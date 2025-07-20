04c1fa1884264abed8f95c13aee49ae753e00b49
sujal <sujalkesharwani220@gmail.com>
2025-07-20 14:00:03 +0530

    09: projects

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -171,7 +171,7 @@ export const codeAgentFunction = inngest.createFunction(
       if (isError) {
         return await prisma.message.create({
           data: {
-            // projectId: event.data.projectId,
+            projectId: event.data.projectId,
             content: "Something went wrong. Please try again.",
             role: "ASSISTANT",
             type: "ERROR",
@@ -180,7 +180,7 @@ export const codeAgentFunction = inngest.createFunction(
       }
       return await prisma.message.create({
         data: {
-          // projectId: event.data.projectId,
+          projectId: event.data.projectId,
           content: result.state.data.summary,
           role: "ASSISTANT",
           type: "RESULT",
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,8 +1,10 @@
 import { messagesRouter } from "@/modules/messages/procedures";
+import { projectsRouter } from "@/modules/projects/procedures";
 import { createTRPCRouter } from "@/trpc/init";
 
 export const appRouter = createTRPCRouter({
   messages: messagesRouter,
+  projects: projectsRouter,
 });
 
 export type AppRouter = typeof appRouter;
