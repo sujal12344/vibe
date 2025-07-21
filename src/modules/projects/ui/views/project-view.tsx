340f462c77b01b9fd7f7bc654943e7c1dc9d004b
sujal <sujalkesharwani220@gmail.com>
2025-07-21 12:56:59 +0530

    11: Project header

--- src/modules/projects/ui/views/project-view.tsx
+++ src/modules/projects/ui/views/project-view.tsx
@@ -7,13 +7,20 @@ import {
 } from "@/components/ui/resizable";
 
 import { MessagesContainer } from "../components/messages-container";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
+import { Fragment } from "@prisma/client";
+import { ErrorBoundary } from "react-error-boundary";
+import { ProjectHeader } from "../components/project-header";
 
 interface Props {
   projectId: string;
 }
 
 export const ProjectView = ({ projectId }: Props) => {
+  // const { has } = useAuth();
+  // const hasProAccess = has?.({ plan: "pro" });
+  const [activeFragment, setActiveFragment] = useState<Fragment | null>(null);
+  const [tabsState, setTabsState] = useState<"preview" | "code">("preview");
   return (
     <div className="h-screen">
       <ResizablePanelGroup direction="horizontal">
@@ -22,9 +29,20 @@ export const ProjectView = ({ projectId }: Props) => {
           minSize={20}
           className="flex flex-col min-h-0"
         >
-          <Suspense fallback={<p>Loading...</p>}>
-            <MessagesContainer projectId={projectId} />
-          </Suspense>
+          <ErrorBoundary fallback={<p>Project header error!!</p>}>
+            <Suspense fallback={<p>LOading Project....</p>}>
+              <ProjectHeader projectId={projectId} />
+            </Suspense>
+          </ErrorBoundary>
+          <ErrorBoundary fallback={<p>Messages container error!!</p>}>
+            <Suspense fallback={<p>Loading...</p>}>
+              <MessagesContainer
+                activeFragment={activeFragment}
+                setActiveFragment={setActiveFragment}
+                projectId={projectId}
+              />
+            </Suspense>
+          </ErrorBoundary>
         </ResizablePanel>
         <ResizableHandle></ResizableHandle>
         <ResizablePanel defaultSize={65} minSize={50}>
